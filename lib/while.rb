6415f804b43e39f3bf6d1d7b7fa87a2c5da4d401
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-12 19:12:34 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,9 +2,9 @@
 # require "../lib/while.rb"
 def using_while
   levitation_force = 6
-  while using_while <= 10
+  while levitation_force <= 10
     levitation_force += 1
-  puts "Wingardium Liviosa"
+    puts "Wingardium Leviosa"
   end
 end
-puts "Wingardium Liviosa"
+puts "Wingardium Leviosa"
